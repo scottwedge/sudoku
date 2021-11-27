f859bc2f8af5a2fd6bc13b5bf96c803f9e90caab
Scott Wedge <scott.wedge@gmail.com>
2021-11-26 20:49:35 -0500

    First function welcomes user

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,10 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
 # Script to solve simple Sudoku puzzle
 
-# 
+def greet_user():    # Greet user
+    print("Welcome to my Sudoku solving application")
+
+
+# Main code
+greet_user() 
