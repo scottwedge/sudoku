e161471e8a5451ff70d649c80b4b01edab092bca
Scott Wedge <scott.wedge@gmail.com>
2021-11-30 18:12:49 -0500

    Fix minor display issues with text, grid without lines and grid with lines; Remove last grid display since not needed

--- mod_intdiv.py
+++ mod_intdiv.py
@@ -14,7 +14,7 @@ SPACE = " "     # have space on either side of value to make reading grid easier
 # Functions
 def greet_user():    # Greet user
     print("Welcome to my modulo and integer division calculation application", end="")
-    print("for {} by {} puzzle".format(FULL_SIDE, FULL_SIDE))
+    print(" for {} by {} puzzle".format(FULL_SIDE, FULL_SIDE))
 
 def create_grid(FULL_SIDE):   # Initialize list of values from 0 to 80
     grid_list = []        
@@ -26,7 +26,7 @@ def show_grid(grid, FULL_SIDE):    # display list values in grid format without 
     print()  # blank line
     for row in range(FULL_SIDE):
         for column in range(FULL_SIDE):
-            print("{:2d}".format(grid[row * FULL_SIDE + column]),  sep="", end="")
+            print("{:3d}".format(grid[row * FULL_SIDE + column]),  sep="", end="")
         print() # line break at end of line
 
 def create_row_separating_line_with_intersecting_plus_symbol(FULL_SIDE, ROW_SEP, COL_SEP):  # display with both horizontal and vertical lines "+-+-+-...-+" format
@@ -41,6 +41,7 @@ def show_grid_with_lines(grid, FULL_SIDE, ROW_SEP, COL_SEP):    # Add separator 
         for column in range(FULL_SIDE):
             print("{} {:2d} ".format(COL_SEP, grid[row * FULL_SIDE + column]),  sep="", end="")
         print(COL_SEP) # Add final column separator and default line break at end of line
+    create_row_separating_line_with_intersecting_plus_symbol(FULL_SIDE, ROW_SEP, COL_SEP)  # print bottom-most grid line
 
 
 
@@ -53,12 +54,6 @@ show_grid(grid, FULL_SIDE)
 
 show_grid_with_lines(grid, FULL_SIDE, ROW_SEP, COL_SEP)
 
-print()
-print("Start solving puzzle now.")
-
 
 for j in range(len(grid)):
     pass
-
-print(grid)
-show_grid_with_lines(grid, FULL_SIDE, ROW_SEP, COL_SEP)    # Add separator characters between rows and columns
