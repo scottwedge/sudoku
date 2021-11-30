5d36e4c9c259c6b17c40b7ba2a3674fad4434d55
Scott Wedge <scott.wedge@gmail.com>
2021-11-30 17:47:17 -0500

    Replace 'puzzle' with 'grid'; Display works but first two rows not aligned

--- mod_intdiv.py
+++ mod_intdiv.py
@@ -16,22 +16,22 @@ def greet_user():    # Greet user
     print("Welcome to my modulo and integer division calculation application", end="")
     print("for {} by {} puzzle".format(FULL_SIDE, FULL_SIDE))
 
-def create_grid(FULL_SIDE):
-    grid_list = []         # Initialize grid
+def create_grid(FULL_SIDE):   # Initialize list of values from 0 to 80
+    grid_list = []        
     for count in range(FULL_SIDE ** 2):
         grid_list.append(count)
     return grid_list
 
-def show_grid(grid, FULL_SIDE):    # format known puzzle values into grid to be displayed to user
+def show_grid(grid, FULL_SIDE):    # display list values in grid format without lines 
     print()  # blank line
     for row in range(FULL_SIDE):
         for column in range(FULL_SIDE):
-            print(grid[row * FULL_SIDE + column], " ",  sep="", end="")
+            print("{} ".format(grid[row * FULL_SIDE + column]),  sep="", end="")
         print() # line break at end of line
 
-def create_row_separating_line_with_intersecting_plus_symbol(FULL_SIDE, ROW_SEP, COL_SEP):  # "+-+-+-...-+" format
+def create_row_separating_line_with_intersecting_plus_symbol(FULL_SIDE, ROW_SEP, COL_SEP):  # display with both horizontal and vertical lines "+-+-+-...-+" format
     for j in range(FULL_SIDE):
-        print("+", ROW_SEP, ROW_SEP, ROW_SEP, sep="", end="") 
+        print("+", ROW_SEP, ROW_SEP, ROW_SEP, ROW_SEP, sep="", end="") 
     print("+")   # Need new line at end of string of symbols
 
 def show_grid_lines(grid, FULL_SIDE, ROW_SEP, COL_SEP):    # Add separator characters between rows and columns
