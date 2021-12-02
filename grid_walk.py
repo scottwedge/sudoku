c67b537823d1969205108b3bc1117a0bc7b89986
Scott Wedge <scott.wedge@gmail.com>
2021-12-01 21:03:32 -0500

    Improve comments describing list creation; Remove unused code (since was copied from sudoku.py

--- grid_walk.py
+++ grid_walk.py
@@ -20,13 +20,6 @@ def create_puzzle():
     return puzzle
 
 
-def show_grid(puzzle, FULL_SIDE):    # format known puzzle values into grid to be displayed to user
-    print()  # blank line
-    for row in range(FULL_SIDE):
-        for column in range(FULL_SIDE):
-            print(puzzle[row * FULL_SIDE + column], " ",  sep="", end="")
-        print() # line break at end of line
-
 def create_row_separating_line_with_intersecting_plus_symbol(FULL_SIDE, ROW_SEP, COL_SEP):  # "+-+-+-...-+" format
     for j in range(FULL_SIDE):
         print("+", ROW_SEP, ROW_SEP, ROW_SEP, ROW_SEP, sep="", end="") 
@@ -40,30 +33,16 @@ def show_grid_lines(puzzle, FULL_SIDE, ROW_SEP, COL_SEP):    # Add separator cha
             print("{}{}{:2d}{}".format(COL_SEP, SPACE, puzzle[row * FULL_SIDE + column], SPACE),  sep="", end="")
         print(COL_SEP) # Add final column separator and default line break at end of line
 
-def all_values(FULL_SIDE):
-    values = []
-    for count in range(1, FULL_SIDE + 1):
-        values.append([count])    # Add value as single list so can easily check if [k] in [j]
-    return values
-
-def setup_possibles_list(puzzle, values):
-    possibles_list = []   # initialize empty list
-
-    for j in range(len(puzzle)):
-        if puzzle[j] == 0:
-            possibles_list.append(all_values(FULL_SIDE))          # all values possible for blank field
-        else:
-            possibles_list.append([puzzle[j]])       # value is already known so use it as a list of one value
-    return possibles_list
 
 
 def create_list_of_internal_grids(PART_SIDE):  # Create list of internal grid lists for any N x N grid
     list_of_internal_grids = []
-    for s in range(PART_SIDE):
-        for n in range(PART_SIDE):   # move to next grid
+    for s in range(PART_SIDE): # Move down vertically between top left spot in column of left-most internal grids
+                               # So in a 9x9 grid (spots 0-80) move between spots 0, 27 and 54
+        for n in range(PART_SIDE):   # move to next grid to the right 
             internal_grid = []    # Initialize new list
-            for v in range(PART_SIDE):  # Move vertically within grid 
-                for h in range(PART_SIDE): # Move horizontally within grid
+            for v in range(PART_SIDE):  # Move vertically within internal grid 
+                for h in range(PART_SIDE): # Move horizontally within internal grid
                     spot = v * PART_SIDE ** 2 + h
                     spot = spot + n * PART_SIDE
                     spot = spot + s * PART_SIDE ** 3
