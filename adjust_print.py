5a91a82bda4f6bdb716a694c34f5f3d7209dace8
Scott Wedge <scott.wedge@gmail.com>
2021-12-06 21:11:00 -0500

    Practice adjusting column width with variables

---
+++ adjust_print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Adjust column width in print statement
+
+a = 1
+b = 2
+c = 3
+
+print("{a:{b}{c}}".format(a=123, b=0, c=6))
+
+#print("{num:{fill}{width}}".format(num=123, fill='o', width=6))
