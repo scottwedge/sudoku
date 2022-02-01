8b2ea94ff505c12d8674ba2dcb803ad430be9a98
Scott Wedge <scott.wedge@gmail.com>
2022-02-01 16:11:17 -0500

    Add debugging to determine why previously completed easy puzzles now hang

--- sudoku.py
+++ sudoku.py
@@ -331,6 +331,9 @@ def list_count(puzzle):
 
 # Determine if game over or continues 
 def are_we_done(puzzle, loop, last_count):
+    print("Entered fn 'are_we_done'. loop= {}. Last_count = {}".format(loop, last_count))  #DEBUG
+    lc = list_count(puzzle)  #DEBUG
+    print("list_count= {}".format(lc))  #DEBUG
     done = False
     reason = "Continue"
     if last_count == list_count(puzzle):
@@ -340,8 +343,10 @@ def are_we_done(puzzle, loop, last_count):
         done = True
         reason = "Looped maximum of {} times".format(MAX_LOOP)
     if all_grids_resolved(puzzle):
+        print("puzzle done 44")  #DEBUG
         done = True
-        sane = check_puzzle_sanity(puzzle)  # Check if solved puzzle is valid
+        (sane, reason) = check_puzzle_sanity(puzzle)  # Check if solved puzzle is valid
+        print("Sanity = {}. Reason = {}".format(sane, reason))   #DEBUG
         if sane:
             reason = "Puzzle sane. All {} grids resolved".format(len(puzzle))
         else:
@@ -440,8 +445,9 @@ def all_grids_sane(puzzle):
 
 
 def check_puzzle_sanity(puzzle):  # Check if solved puzzle is valid/sane
-    sanity = all_rows_sane(puzzle) and all_columns_sane(puzzle) and all_grids_sane(puzzle)
-    return sanity
+#    sanity = all_rows_sane(puzzle) and all_columns_sane(puzzle) and all_grids_sane(puzzle)
+    (sanity, reason) = all_rows_sane(puzzle)
+    return (sanity, reason)
 
 
 def all_grids_resolved(puzzle):
@@ -898,17 +904,22 @@ def solve_puzzle(puzzle):
         show_adjustable_grid_lines(puzzle, full_side, ROW_SEP, COL_SEP, column_max)    #DEBUG 
     
         current_count = list_count(puzzle)
-        
+       
         progress = find_pairs(puzzle)  
+
         if progress:  # reset loop and count values so looping continues
             loop = 0
             last_count = 100000
     
+        print("HHHHere3")  #DEBUG 
+
         (done, reason) = are_we_done(puzzle, loop, last_count)
     
+        print("HHHHere4")  #DEBUG 
+
         last_count = current_count
     else:
-        print("Game over because {} so try guessing.".format(reason))
+        print("Game over. {}".format(reason))
         progress = find_pairs(puzzle)  
 
     return (reason, puzzle)
@@ -951,6 +962,7 @@ show_adjustable_grid_lines(puzzle, full_side, ROW_SEP, COL_SEP, column_max)    #
 
 count = count_total_possible_values(puzzle)   # Count all the known and unknown values in the puzzle
 print("Total values count in the puzzle is {}.".format(count_total_possible_values(puzzle)))
+print("HHere")   #DEBUG
 
 if count > len(puzzle):  # Decide how to proceed if there are still unresolved grids
     print()
@@ -979,7 +991,7 @@ if count > len(puzzle):  # Decide how to proceed if there are still unresolved g
         puzzle = get_user_guess(puzzle) 
         (reason, puzzle) = solve_puzzle(puzzle)
         (sane, reason) = check_puzzle_sanity(puzzle)  # Check if solved puzzle is valid/sane
-        print("puzzle sanity is {}".format(sane))
+        print("puzzle sanity is {} because {}".format(sane, reason))
     
     if reply == 5:   # Time trial for 1000 attempts, then calculate worst case if all possibilities needed
         start_num = 0
